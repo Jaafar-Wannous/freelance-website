701f70fa3cd203ad9adefe03daa2d0146cb874e0
mahlaee <mahlaeelSaldabet@gmail.com>
2024-10-15 13:55:33 +0300

    add API Routes

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -17,3 +17,25 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::controller(RegisterController::class)->group(function() {
+    Route::post('register', 'register');
+    Route::post('login', 'login');
+    Route::post('forget-password', 'forgetPassword');
+    Route::post('reset-password', 'resetPassword');
+});
+
+Route::apiResource('messages', MessageController::class)
+->only(['index', 'show']);
+Route::apiResource('notifications', NotificationController::class)
+->only(['index', 'show']);
+Route::apiResource('categories', CategoryController::class)
+->only(['index', 'show']);
+Route::apiResource('services', ServiceController::class)
+->only(['index', 'show']);
+
+Route::middleware('auth:sanctum')->group(function() {
+    Route::apiResource('services', ServiceController::class)
+    ->except(['index', 'show']);
+    Route::apiResource('users', UserController::class);
+});
