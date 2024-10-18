25042222ede882d62369403241094970eb26996a
Jaafar-Wannous <jaafarwannous1@gmail.com>
2024-10-18 20:47:17 +0300

    remove primeng

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,9 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ButtonModule } from 'primeng/button'; // PrimeNG Button module
-import { RippleModule } from 'primeng/ripple';
-import { InputTextModule } from 'primeng/inputtext';
-import { CardModule } from 'primeng/card';
 
 
 import { AppComponent } from './app.component';
@@ -12,8 +8,6 @@ import { HomeComponent } from './home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
-import { AutoCompleteModule } from 'primeng/autocomplete';
-import { PasswordModule } from 'primeng/password';
 
 @NgModule({
   declarations: [
@@ -23,17 +17,10 @@ import { PasswordModule } from 'primeng/password';
   ],
   imports: [
     BrowserModule,
-    ButtonModule,
-    BrowserModule,
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    RippleModule,
-    InputTextModule,
-    CardModule,
-    AutoCompleteModule,
-    PasswordModule
   ],
   providers: [],
   bootstrap: [AppComponent]
