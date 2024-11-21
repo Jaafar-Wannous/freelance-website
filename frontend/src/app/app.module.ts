fbec0d84fb5d226c7e8621b2c14621cbc5ed7ebe
Jaafar-Wannous <jaafarwannous1@gmail.com>
2024-11-21 16:04:30 +0300

    add profile

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FilePondModule } from 'ngx-filepond';
 
@@ -10,22 +10,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
+import { StarRatingComponent } from './components/star-rating/star-rating.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
-import { ServicesComponent } from './services/services.component';
 import { AddServiceComponent } from './services/add-service/add-service.component';
-import { StarRatingComponent } from './components/star-rating/star-rating.component';
+import { ServicesComponent } from './services/services.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 import * as filePond from 'filepond';
-import * as FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type';
-import * as FilePondPluginFileValidateSize from 'filepond-plugin-file-validate-size';
-import * as FilePondPlugingImageValidateSize from 'filepond-plugin-image-validate-size';
 import * as FilePondPlugingFileEncode from 'filepond-plugin-file-encode';
+import * as FilePondPluginFileValidateSize from 'filepond-plugin-file-validate-size';
+import * as FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type';
+import * as FilePondPlugingImageValidateSize from 'filepond-plugin-image-validate-size';
 
 filePond.registerPlugin(FilePondPluginFileValidateType);
 filePond.registerPlugin(FilePondPluginFileValidateSize);
 filePond.registerPlugin(FilePondPlugingImageValidateSize);
 filePond.registerPlugin(FilePondPlugingFileEncode);
+
 
 @NgModule({
   declarations: [
@@ -35,9 +37,9 @@ filePond.registerPlugin(FilePondPlugingFileEncode);
     ServicesComponent,
     RegisterComponent,
     LoginComponent,
+    UserProfileComponent,
     AddServiceComponent,
-    StarRatingComponent,
-
+    StarRatingComponent
   ],
   imports: [
     BrowserModule,
@@ -46,12 +48,9 @@ filePond.registerPlugin(FilePondPlugingFileEncode);
     HttpClientModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    FilePondModule
+    FilePondModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
-  schemas: [
-    CUSTOM_ELEMENTS_SCHEMA
-  ]
 })
 export class AppModule { }
