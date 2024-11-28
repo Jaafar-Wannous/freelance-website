b3b0caf5a1f1a0c4e1732d386d4b81c504c6c44e
Jaafar-Wannous <jaafarwannous1@gmail.com>
2024-11-28 23:24:05 +0300

    develop guard & fix image-user bug

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,23 +2,24 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
-import { authGuard } from './guards/auth.guard';
 import { HomeComponent } from './home/home.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { AddServiceComponent } from './services/add-service/add-service.component';
 import { VerifyEmailComponent } from './auth/verify-email/verify-email.component';
+import { authGuard, guestGuard } from './guards/auth-guards';
+
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'register', component: RegisterComponent, canActivate: [authGuard] },
-  { path: 'verify-email', component: VerifyEmailComponent, canActivate: [authGuard] },
-  { path: 'login', component: LoginComponent, canActivate: [authGuard] },
-  { path: 'profile', component: UserProfileComponent},
-  { path: 'add-service', component: AddServiceComponent},
+  { path: 'register', component: RegisterComponent, canActivate: [guestGuard] },
+  { path: 'verify-email', component: VerifyEmailComponent, canActivate: [guestGuard] },
+  { path: 'login', component: LoginComponent, canActivate: [guestGuard] },
+  { path: 'profile', component: UserProfileComponent, canActivate: [authGuard] },
+  { path: 'add-service', component: AddServiceComponent, canActivate: [authGuard] },
 
-
-  {path: '**', redirectTo: ''}
+  { path: '**', redirectTo: '' }
 ];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
