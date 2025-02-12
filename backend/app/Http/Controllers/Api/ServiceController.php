993366b18971babfb1040ea9acdb5d1fff7d56df
mahlaee <mahlaeelSaldabet@gmail.com>
2025-02-12 15:54:36 +0300

    Add Reply && edit ServiceController and User Model

--- backend/app/Http/Controllers/Api/ServiceController.php
+++ backend/app/Http/Controllers/Api/ServiceController.php
@@ -71,7 +71,7 @@ class ServiceController extends Controller
         ->with(['user', 'category', 'review'])
         ->get();
 
-        $serv->load(['review.writer', 'review.recipient']);
+        $serv->load(['review.writer', 'review.recipient', 'review.replies', 'review.replies.user', 'category.services.review', 'category.services.user']);
 
         return response()->json([
             'success' => true,
