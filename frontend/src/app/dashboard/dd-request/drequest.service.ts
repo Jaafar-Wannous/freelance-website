e8741d954dcac128e49fc2663381657269a7e522
mahlaee <mahlaeelSaldabet@gmail.com>
2025-02-27 10:43:17 +0300

    add token

--- frontend/src/app/dashboard/dd-request/drequest.service.ts
+++ frontend/src/app/dashboard/dd-request/drequest.service.ts
@@ -24,33 +24,45 @@ export class DashboardRequestService {
 
   makeRequest(data: any) {
         const token = localStorage.getItem('token');
-
+    
         const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
 
         return this.http.post(this.apiUrl, data, { headers });
   }
 
-createService(data: any) {
-  return this.http.post('http://127.0.0.1:8000/api/services', data);
-}
-
-updateService(serviceId: number, data: any) {
-  return this.http.put(`http://127.0.0.1:8000/api/services/${serviceId}`, data);
-}
-
-verifyPhone(userId: number, data: any) {
-  return this.http.put(`http://127.0.0.1:8000/api/users/${userId}/verify-phone`, data);
-}
-
-verifypId(userId: number, data: any) {
-  return this.http.put(`http://127.0.0.1:8000/api/users/${userId}/verify-pid`, data);
-}
-
-createRequestService(data: any) {
-return this.http.post(`request`, data);
-}
-
-  deleteRequest(id: number) {
-    return this.http.delete(`http://127.0.0.1:8000/api/dashboard-requests/${id}`);
+  createService(data: any) {
+    const token = localStorage.getItem('token_admin');
+    
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    return this.http.post('http://127.0.0.1:8000/api/services', data, {headers});
   }
+  
+  updateService(serviceId: number, data: any) {
+    const token = localStorage.getItem('token_admin');
+    
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    return this.http.put(`http://127.0.0.1:8000/api/services/${serviceId}`, data, {headers});
+  }
+  
+  verifyPhone(userId: number, data: any) {
+    const token = localStorage.getItem('token_admin');
+    
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    return this.http.put(`http://127.0.0.1:8000/api/users/${userId}/verify-phone`, data, {headers});
+  }
+  
+  verifypId(userId: number, data: any) {
+    const token = localStorage.getItem('token_admin');
+    
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    return this.http.put(`http://127.0.0.1:8000/api/users/${userId}/verify-pid`, data, {headers});
+  }
+  
+  
+    deleteRequest(id: number) {
+      const token = localStorage.getItem('token_admin');
+    
+      const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+      return this.http.delete(`http://127.0.0.1:8000/api/dashboard-requests/${id}`, {headers});
+    }
 }
